2097309b4c6ded5f0a196b171f9dbf50646a5a5c
Staffan Olsson <staffan@repos.se>
2015-06-26 12:04:08 +0200

    The motivation

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+
+### Local clock
+
+From https://www.youtube.com/watch?v=NSTZ4mIv_wk at 18 minutes (with context from 8th minute):
+
+"A local clock just starts at zero and every operation increments that local clock by one"
---
+++ test/opid-spec.js
@@ -0,0 +1,63 @@
+
+describe("OPID", function() {
+
+  describe("configuration", function() {
+
+    it("Could make good use of instantiation with config, unlike node-uuid that takes config for each call" , function() {
+
+    });
+
+  });
+
+  describe("#uuid", function() {
+
+    it("Increments the local clock", function() {
+    });
+
+    it("Is a variant of UUID v1", function() {
+
+    });
+
+    it("UUID v1 already identifies clients", function() {
+
+    });
+
+    it("Is a feature that ID contains a client's clock timestamp", function() {
+
+    });
+
+    it("Also contains the op")
+
+  });
+
+  describe("#woot", function() {
+
+    it("Increments the local clock", function() {
+    });
+
+    it("Returns ID sequence per client", function() {
+
+    });
+
+    it("Uses the same ID as uuid (hence the use for instantiation)", function() {
+
+    });
+
+    it("Varies on nothing but the local clock part", function() {
+
+    });
+
+  });
+
+  describe("#op", function() {
+
+    it("Increments the local clock", function() {
+    });
+
+    it("Is only unique within a session", function() {
+
+    });
+
+  });
+
+});
